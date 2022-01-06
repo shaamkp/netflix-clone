b2432b93b550433d4ee5772f521b421a58b28f63
unknown <shyam.steyp@gmail.com>
2022-01-06 11:58:41 +0530

    nav banner and trailer are updated

--- src/components/screens/Nav.js
+++ src/components/screens/Nav.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import netflix_logo from '../../assets/logo.png'
 
 export default function Nav() {
+  const[show, hanleShow]=useState(false); 
+
+  useEffect(() =>{
+    window.addEventListener("scroll", () =>{
+      if(window.scrollY > 100){
+        hanleShow(true);
+      }else hanleShow(false)
+    });
+    return () =>{
+      window.removeEventListener("scroll");
+    };
+  }, []);
     return (
-      <div className="Nav">
+      <div className={`Nav ${show && 'nav_black'}`}>
         <img src={netflix_logo} alt="Image" className="navLogo" />
       </div>
     );
--- src/components/screens/Row.js
+++ src/components/screens/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "../../axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 export default function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl,setTrailerUrl]=useState("")
 
   useEffect(() => {
     async function fetchData() {
@@ -16,6 +19,27 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height : "390",
+    width : "100%",
+    playerVars :{
+      autoplay:'1',
+    }
+  }
+
+  const handleClick = (movie) =>{
+    if(trailerUrl){
+      setTrailerUrl("");
+    }else{
+      movieTrailer(movie?.name || "")
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get('v'))
+
+      }).catch(error => console.log(error));
+    }
+  }
 
   console.log(movies);
   return (
@@ -29,10 +53,12 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
             className={`rowposter ${isLargeRow && "row_posterLarge"}`}
           />
         ))}
       </RowPosters>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
